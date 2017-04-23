10b2ca0e2b457862c96dfd91bd026efb7915c878
jerradmonagan <jerradmonagan@gmail.com>
2017-04-23 15:01:29 -0400

    Uploaded completed project for historical purposes
    Project was previously completed in an older course

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.txtShowMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.AllowDrop = true;
+            this.btnClickMe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClickMe.Location = new System.Drawing.Point(85, 140);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(108, 35);
+            this.btnClickMe.TabIndex = 0;
+            this.btnClickMe.Text = "Click Me";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // txtShowMessage
+            // 
+            this.txtShowMessage.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShowMessage.Location = new System.Drawing.Point(65, 61);
+            this.txtShowMessage.Name = "txtShowMessage";
+            this.txtShowMessage.Size = new System.Drawing.Size(152, 26);
+            this.txtShowMessage.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 268);
+            this.Controls.Add(this.txtShowMessage);
+            this.Controls.Add(this.btnClickMe);
+            this.Name = "Form1";
+            this.Text = "Hello World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.TextBox txtShowMessage;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # COP2010-HW1
 Homework 1 for COP2010 C# OOP 
+COP2010 Homework Assignment #1 
+5 pts – Due by start of class on 9/1 
+This is a very easy assignment and it is meant to give you an opportunity to experiment in Visual Studio.  You will need to create your own version of the Hello World application we went over in class and submit to canvas.   
+Objectives: 
+•	Use Visual Studio to create a windows form application 
+•	Learn how to zip and submit a file to canvas 
+•	Learn how to change different properties on the form 
+Requirements 
+1.	Read the projects and assignments requirements pdf in the course documents section of canvas.  You only need to follow part 1 on naming your project and part 2 on documentation.  You do not need to do parts 3 and 4 but you can try if you want.  
+  
+2.	Create a windows form application and name it as described in the assignments requirements.  Add a text box and button. 
+ 
+3.	When you press the button it will display the message “Hello World” in the text box.   
+ 
+4.	Change some aspects of the form from the default settings e.g. button text, font size, color, form name, font color, form color, etc. 
+ 
+5.	Zip the entire project file and submit to canvas. 
+ Possible GUI Design, but you can change to experiment with the different settings 
+               
+ 
+After clicking the button it should display Hello World in the text box.  
